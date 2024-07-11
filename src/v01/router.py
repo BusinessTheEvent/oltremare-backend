ea9c8d2e2469b786113de07e9a5f011fb87c13b6
RICCARDO COMELLATO <885952@stud.unive.it>
2024-07-11 16:58:06 +0200

    chore: Update database schema for chief table

--- src/v01/router.py
+++ src/v01/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from src.auth.models import User
-from src.v01.models import Booking, Teacher, Student
+from src.v01.models import Booking, Teacher, Student, TeacherSchoolSubject, Subject
 from src.databases.db import get_db
 from src.default_logger import get_custom_logger
 from src.schemas.v01_schemas import BookingSchema
@@ -32,34 +32,143 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
 #accessibile solo da admin
 @router.get("/booking/all")
 def get_all_booking(db: Session = Depends(get_db)):
-    booking = db.query(User).filter(User.id == user_id).all()
+    booking = db.query(Booking).all()
     return booking
 
 #get all teacher
 @router.get("/booking/all_teacher")
 def get_all_booking(db: Session = Depends(get_db)):
-    return db.query(User,Teacher).join(Teacher, User.id == Teacher.id).all()
+    # Query and join to get necessary data
+    teachers_bookings = db.query(Teacher, Booking).join(Booking, Teacher.id == Booking.id_teacher).all()
 
-#get all teacher groub by subject
-@router.get("/booking/all_teacher_by_subject")
-def get_all_booking(db: Session = Depends(get_db)):
-    return db.query(User,Teacher).join(Teacher, User.id == Teacher.id).all()
+    # Prepare the response in a serializable format
+    result = []
+    for teacher, booking in teachers_bookings:
+        teacher_data = {
+            'id': teacher.id,
+            # Include other attributes of Teacher model if needed
+        }
+        booking_data = {
+            'id_booking': booking.id_booking,
+            'id_student': booking.id_student,
+            'id_teacher': booking.id_teacher,
+            'id_school_grade': booking.id_school_grade,
+            'id_subject': booking.id_subject,
+            'start_datetime': booking.start_datetime,
+            'end_datetime': booking.end_datetime,
+            'duration': booking.duration,
+            'notes': booking.notes,
+            'attended': booking.attended,
+            'insert_id_user': booking.insert_id_user,
+            'insert_date': booking.insert_date,
+            'insert_time': booking.insert_time
+        }
+        result.append({
+            'teacher': teacher_data,
+            'booking': booking_data
+        })
+
+    return result
+
+#get teacher group by subject
+@router.get("/booking/get_teacher_by_school_and_subject/{id_subject}{id_school_grade}")
+def get_all_booking(id_subject: int, id_school_grade: int,db: Session = Depends(get_db)):
+    # Query and join to get necessary data
+    list = db.query(Teacher, TeacherSchoolSubject, Subject)\
+                .join(Teacher, Teacher.id == TeacherSchoolSubject.id)\
+                .join(Subject, Subject.id_subject == TeacherSchoolSubject.id_subject)\
+                .filter(Subject.id_subject == id_subject, TeacherSchoolSubject.id_school_grade == id_school_grade)\
+                .all()
+    
+    #TODO: finish and test this
+    print(list)
+    return
+
+# SELECT teacher.id AS teacher_id, teacher_school_subject.id AS teacher_school_subject_id, teacher_school_subject.id_school_grade AS teacher_school_subject_id_school_grade, teacher_school_subject.id_subject AS teacher_school_subject_id_subject, subjects.id_subject AS subjeccts_id_subject, subjects.name AS subjects_name
+# FROM teacher_school_subject JOIN teacher ON teacher.id = teacher_school_subject.id JOIN teacher_school_subject ON subjects.id_subject = teacher_school_subject.id_subject, subjects
+# WHERE subjects.id_subject = 1 AND teacher_school_subject.id_school_grade = 1
+
+#get all teacher group by subject
+# @router.get("/booking/all_teacher_by_subject")
+# def get_all_booking(db: Session = Depends(get_db)):
+#     # Query and join to get necessary data
+#     teacher_by_subject = db.query(User,Teacher).join(Teacher, User.id == Teacher.id).all()
+
+#     # Prepare the response in a serializable format
+#     result = []
+#     for student, booking in teacher_by_subject:
+#         students_data = {
+#             'id': student.id,
+#             # Include other attributes of Teacher model if needed
+#         }
+#         booking_data = {
+#             'id_booking': booking.id_booking,
+#             'id_student': booking.id_student,
+#             'id_teacher': booking.id_teacher,
+#             'id_school_grade': booking.id_school_grade,
+#             'id_subject': booking.id_subject,
+#             'start_datetime': booking.start_datetime,
+#             'end_datetime': booking.end_datetime,
+#             'duration': booking.duration,
+#             'notes': booking.notes,
+#             'attended': booking.attended,
+#             'insert_id_user': booking.insert_id_user,
+#             'insert_date': booking.insert_date,
+#             'insert_time': booking.insert_time
+#         }
+#         result.append({
+#             'student': students_data,
+#             'booking': booking_data
+#         })
+
+#     return result
 
 #get all student
 @router.get("/booking/all_student")
-def get_all_booking(db: Session = Depends(get_db)):    
-    return db.query(User,Student).join(Student, User.id == Student.id).all()
+def get_all_booking(db: Session = Depends(get_db)):
+    # Query and join to get necessary data
+    students_bookings = db.query(Student, Booking).join(Booking, Student.id == Booking.id_student).all()
+
+    # Prepare the response in a serializable format
+    result = []
+    for student, booking in students_bookings:
+        students_data = {
+            'id': student.id,
+            # Include other attributes of Teacher model if needed
+        }
+        booking_data = {
+            'id_booking': booking.id_booking,
+            'id_student': booking.id_student,
+            'id_teacher': booking.id_teacher,
+            'id_school_grade': booking.id_school_grade,
+            'id_subject': booking.id_subject,
+            'start_datetime': booking.start_datetime,
+            'end_datetime': booking.end_datetime,
+            'duration': booking.duration,
+            'notes': booking.notes,
+            'attended': booking.attended,
+            'insert_id_user': booking.insert_id_user,
+            'insert_date': booking.insert_date,
+            'insert_time': booking.insert_time
+        }
+        result.append({
+            'student': students_data,
+            'booking': booking_data
+        })
+
+    return result
+
 
 #accessibile da tutti
 @router.get("/booking/{id_booking}")
 def get_booking(id_booking: int, db: Session = Depends(get_db)):
-    book = db.query(Booking).filter(Booking.id == id_booking).first()
+    book = db.query(Booking).filter(Booking.id_booking == id_booking).first()
     return book
 
 #prenotazioni di uno studente
 @router.get("/booking/{id_student}")
 def get_student_booking(id_student: int, db: Session = Depends(get_db)):
-    book = db.query(Booking).filter(Booking.id_student == id_student).first()
+    book = db.query(Booking).filter(Booking.id_student == id_student).all()
     return book
 
 #prenotazioni di un insegnante
